f44b112155d3ded7dce699f811fcd796b8ad4181
techswarn <tech.swarns@gmail.com>
2023-08-10 14:17:33 +0530

    code update

--- django_app/settings.py
+++ django_app/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.getenv("DEBUG", "False") == "true"
 #DEBUG = os.getenv("DEBUG")
-DEBUG = True
+print(os.getenv("DEBUG"))
+DEBUG = False
 print(DEBUG)
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
