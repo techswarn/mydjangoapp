3a72919196a352f92d6551727686828515e5d280
techswarn <tech.swarns@gmail.com>
2023-08-10 15:35:15 +0530

    code update

--- django_app/settings.py
+++ django_app/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.getenv("DEBUG", "False") == "true"
-DEBUG = True
+DEBUG = False
 
 print(DEBUG)
 
