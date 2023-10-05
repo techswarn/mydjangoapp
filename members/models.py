ec19a94944ab6e8cb3d057f514baa41805a453b6
techswarn <tech.swarns@gmail.com>
2023-10-05 15:35:52 +0530

    code update

--- members/models.py
+++ members/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
+  email= models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
