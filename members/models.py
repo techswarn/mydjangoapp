8e2692e63d71b0041fd781c4ea844a0b1733de79
techswarn <tech.swarns@gmail.com>
2023-12-02 15:44:47 +0530

    code update

--- members/models.py
+++ members/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
@@ -6,3 +7,4 @@ class Member(models.Model):
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
+  updated_date = models.DateField(default=date.today)
