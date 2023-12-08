337f0233952fea33d14288955962121cdba62b10
techswarn <tech.swarns@gmail.com>
2023-12-08 11:05:31 +0530

    code update

--- members/models.py
+++ members/models.py
@@ -3,8 +3,8 @@ from datetime import date
 
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
-#  email= models.CharField(default='s.swarn619@gmail.com', max_length=255)
+  #email= models.CharField(default='s.swarn619@gmail.com', max_length=255)
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
-  joined_date = models.DateField(default=date.today)
+  joined_date = models.DateField(null=True)
   updated_date = models.DateField(default=date.today)
