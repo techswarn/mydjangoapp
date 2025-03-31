3e0a75fd188cdd42395e4ee99b049be996e3a6af
techswarn <tech.swarns@gmail.com>
2025-03-31 11:55:13 +0530

    updated members modal

--- members/models.py
+++ members/models.py
@@ -5,7 +5,7 @@ class Member(models.Model):
   firstname = models.CharField(max_length=255)
   username = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
-  address = models.CharField(max_length=255)
+  email = models.EmailField(max_length=255, default=None)
   phone = models.IntegerField(null=True)
   pincode = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
