b0fadcdccb7b77881af99798c51e1ef2f0bb44bb
techswarn <tech.swarns@gmail.com>
2025-03-31 13:41:23 +0530

    code update

--- polls/models.py
+++ polls/models.py
@@ -9,5 +9,5 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    newchecked = models.BooleanField(default=False)
+    checked = models.BooleanField(default=False)
     votes = models.IntegerField(default=0)
