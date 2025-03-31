4d7761b95560defbc2fde16e7bae92a03f9d4a1b
techswarn <tech.swarns@gmail.com>
2025-03-31 13:44:26 +0530

    added a model changes

--- polls/models.py
+++ polls/models.py
@@ -10,4 +10,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     checked = models.BooleanField(default=False)
+    selected = models.BooleanField(default=False)
     votes = models.IntegerField(default=0)
