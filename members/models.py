9d6558373af064afc89d78da34a40b6096019cc6
techswarn <tech.swarns@gmail.com>
2025-04-01 21:04:38 +0530

    code update

--- members/models.py
+++ members/models.py
@@ -6,6 +6,7 @@ class Member(models.Model):
   username = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
   location = models.CharField(max_length=255, default="India")
+  email = models.EmailField(max_length=255, unique=True, default=None)
   phone = models.IntegerField(null=True)
   pincode = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
--- run.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-echo "Dejango deployment" && gunicorn --worker-tmp-dir /dev/shm django_app.wsgi && python3 manage.py migrate
